13529ada55590eb80d12e19ecbb905bf8a7e5512
sara saleh alofi <saraalawfi1995@gmail.com>
2019-10-16 16:22:21 +0300

    Add authedUser,questions,users reducers

---
+++ wouldyourather/src/reducers/questions.js
@@ -0,0 +1,33 @@
+import { ADD_QUESTION_ANSWER, ADD_QUESTION, RECEIVE_QUESTIONS} from '../actions/questions'
+
+
+export function questions(state={},action){
+    switch(action.type){
+        case RECEIVE_QUESTIONS:
+            return{
+                ...state,
+                ...action.questions
+            }
+        case ADD_QUESTION:
+            return{
+                ...state,
+                [action.question.id]: action.question
+
+            }
+        case ADD_QUESTION_ANSWER:
+            return {
+             ...state,
+             [action.qid]:{
+                 ...state[action.qid],
+                 [action.answer]:{
+                     ...state[action.qid][action.answer],
+                     votes: state[action.qid][action.answer].votes.concat([action.authedUser])
+                 }
+             }
+
+            }
+        default:
+        return state
+    }
+   
+}
